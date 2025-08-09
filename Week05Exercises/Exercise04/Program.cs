8fce16e4e459cf64a6f581eccad5c60a0786cf98
pascal-maker <pascal-musa@hotmail.com>
2025-08-09 21:21:32 +0200

    easy

---
+++ Week05Exercises/Exercise04/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//creating a list
+
+class Countries
+{
+    public static void Main()
+    {      //exercise1
+        {
+            List<string> Countries = new List<string>() { "London", "Paris", "Milan", "New York", "Los Angeles" };
+            foreach (var country in Countries)
+            {
+                if (country.Contains('L'))
+                {
+                    Console.WriteLine($"This  City {country} starts with a L ");
+                }
+            }
+
+
+
+        }
+
+
+
+        //exercise2
+        {
+            List<string> Cities = new List<string>() { "London", "Paris", "Milan", "New York", "Los Angeles" };
+            foreach (var city in Cities)
+            {
+                if (city.StartsWith('L') && city.EndsWith('n'))
+                {
+                    Console.WriteLine($"This  City {city} starts with a L and ends with a n ");
+                }
+            }
+
+
+
+        }
+
+
+        //exercise3
+
+
+         {
+            List<string> Cities = new List<string>() { "London", "Paris", "Milan", "New York", "Los Angeles" };
+            foreach (var city in Cities)
+            {
+                if (city.Length == 6)
+                {
+                    Console.WriteLine($"This  City {city} has extactly 6 characters.");
+                }
+            }
+
+
+
+        }
+        
+
+        
+        
+
+
+
+
+
+        
+
+    }
+
+}
