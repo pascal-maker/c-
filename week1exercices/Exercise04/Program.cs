808c9492969beb069e24ed710cd9fe453427da7c
pascal-maker <pascal-musa@hotmail.com>
2025-08-13 15:53:27 +0200

    yes uitleg

--- week1exercices/Exercise04/Program.cs
+++ week1exercices/Exercise04/Program.cs
@@ -1,15 +1,16 @@
-﻿List<string> list_favorite_colors_children = new List<string>() 
-{ "green", "yellow", "pink", "blue", "red", "green", "pink", "yellow", "yellow", "black", "pink", "brown" };
-//call the countColors function and store the result in a dictonary
+﻿List<string> list_favorite_colors_children = new List<string>() { "green", "yellow", "pink", "blue", "red", "green", "pink", "yellow", "yellow", "black", "pink", "brown" };
+//we starten met onze list
 var colorCount = CountColors(list_favorite_colors_children);
-// print the results
+//we maken de varibale var colountcount aan is gelijk aan CountCOlors(list_favoirte_childeren) Countcolors is hier een methode naam
 PrintColorCounts(colorCount);
+//printfunvtie met als parameter colorCount
 
-//function to count occurences of each color
-
-static Dictionary<string,int> CountColors(List<string>colors)
+static Dictionary<string,int> CountColors(List<string> colors)
+//we maken een dictionary aan genaamnd COCuntcolor die besatat uit nse ist<string> colors
 {
     var colorCount = new Dictionary<string, int>();
+    //onze variabele colorCOunt wordt dan een nieuw dicrinary<string,int>
+    // dan doen we voor elke kleur in de lisjt kleur van count colors 
     foreach (var color in colors)
     {
         if (colorCount.ContainsKey(color))
@@ -19,12 +20,17 @@ static Dictionary<string,int> CountColors(List<string>colors)
 
     }
     return colorCount;
+    // onze variablele colorcunt is ene dtiicnary geworden dus die kan de jeu oprvagen in onze forloop genamad kleur al die er is dan dan tellen wecolorOCunr[color op anders wordt eht 1]
+
 }
-//function to print the color counts
+
 static void PrintColorCounts(Dictionary<string,int> colorCount)
+// methode printcoloruntcouns aaanrpen die betatauti dciinary<string,int colorOCunt
 {
     foreach (var entry in colorCount)
+    //foreach aaanropen in colorcount 
     {
-        Console.WriteLine($" The Color {entry.Key} appears {entry.Value} time{(entry.Value > 1 ? "s" : "")}");
+        Console.WriteLine($"The color {entry.Key} appears {entry.Value} time{(entry.Value > 1 ? "s" : "")}");
+        // printen key verschijtn zoveel keer mer waarden als entry.value>1 dn een dan krijgt time er een s bij of miet
     }
 }
