53c8df1939979fd5bf6fc896901d8328e83e215e
pascal-maker <pascal-musa@hotmail.com>
2025-08-13 19:16:04 +0200

    yes commentaar

--- week1exercices/Exercise07/Program.cs
+++ week1exercices/Exercise07/Program.cs
@@ -1,23 +1,25 @@
 ﻿Dictionary<string, int> scores = new Dictionary<string, int> { { "Jan", 3 }, { "Paul", 2 }, { "Jef", 5 }, { "Bram", 0 }, { "Pieter", 4 }, { "Koen", 0 } };
- DisplayGrades(scores);
+//functie met paramter opmaken
+DisplayGrades(scores);
+//functie aanroepen
+static void DisplayGrades(Dictionary<string,int> scores)
+{
 
-static void DisplayGrades(Dictionary<string, int> scores)
-{ 
-
-    foreach ( var score in scores)
+    foreach (var score in scores)
+    //hier nog loopen
     {
 
         switch (score.Value)
 
-        {   
-             case 0:
+        {
+            case 0:
                 Console.WriteLine($" {score.Key} ->Insufficient");
                 break;
             case 1:
                 Console.WriteLine($" {score.Key} ->Insufficient");
                 break;
             case 2:
-                Console.WriteLine($" {score.Key} ->Insufficient");
+                Console.WriteLine($" {score.Key} ->Weak");
                 break;
 
             case 3:
@@ -29,17 +31,19 @@ static void DisplayGrades(Dictionary<string, int> scores)
                 Console.WriteLine($" {score.Key} ->Average");
                 break;
 
+
             case 5:
-                Console.WriteLine($" {score.Key} ->ok");
+                Console.WriteLine($" {score.Key} ->Ok");
                 break;
 
-            default :
-                break;    
-   
-}
-    
+            default:
+                Console.WriteLine("exit");
+                break;
+
+
+
+
+
+        }
     }
-    
 }
-
-
