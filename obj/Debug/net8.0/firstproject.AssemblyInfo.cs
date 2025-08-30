62a6d8be012773f3b019121c21276323e872b682
pascal-maker <pascal-musa@hotmail.com>
2025-08-30 15:13:43 +0200

    yes

--- obj/Debug/net8.0/firstproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/firstproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1dbb109b77eda1da8ad598952f73b6b12c087e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fe2e8abcc9e7b8406e84b2d2f1279767edb54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
